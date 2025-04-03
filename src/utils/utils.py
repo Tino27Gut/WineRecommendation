8870f7f2882116eff0622b0c5036f8409f8cdd7e
Tino27Gut <martingut27@gmail.com>
2025-04-03 02:38:56 +0000

    Extended timeout of scraper

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,6 +17,8 @@ def setup_driver(headless=True):
     """ Configura el driver de selenium para WebScraping. """
     options = webdriver.ChromeOptions()
     options.headless = headless
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.set_page_load_timeout(300)
     return driver
+
